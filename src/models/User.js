c91223d087003c3f7a549677413e6f6d493676c7
Camila Wolter <camila0wolter@gmail.com>
2022-11-30 17:12:00 -0300

    requisito 2: criação do medel User

---
+++ src/models/User.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      id: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      underscored: true,
+      tableName: 'users'
+    }
+  );
+  return User;
+}
