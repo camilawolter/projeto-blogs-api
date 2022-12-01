b2a9ec903e94336bf81685ff7453b5334835c13d
Camila Wolter <camila0wolter@gmail.com>
2022-12-01 14:48:06 -0300

    requisito 10: criação do model BlogPost

---
+++ src/models/BlogPost.js
@@ -0,0 +1,41 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define(
+    'BlogPost',
+    {
+      id: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      userId: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+      },
+      published: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updated: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    },
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: 'blog_posts'
+    }
+  );
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'user_id',
+      as: 'users',
+    });
+  };
+
+  return BlogPost;
+}
