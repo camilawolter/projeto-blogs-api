17b4b9d26469cb2c72fe3a761e0140a96e1b6d3c
Camila Wolter <camila0wolter@gmail.com>
2022-12-01 11:25:42 -0300

    requisito 7: criação do model category

---
+++ src/models/Category.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define(
+    'Category',
+    {
+      id: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: 'categories'
+    }
+  );
+  return Category;
+}
